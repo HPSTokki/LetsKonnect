fbbdbaf48634e05ed5f2f05c2d0a2d9b3c5b63e6
sean <mixeuseo@gmail.com>
2024-12-03 01:08:14 +0800

    The file "scholarships.js" no longer has see more content button.

--- js-frntend/scholarships.js
+++ js-frntend/scholarships.js
@@ -17,23 +17,4 @@ document.addEventListener("DOMContentLoaded", function() {
     
 });
 
-function toggleMoreContent() {
-    const moreContent = document.querySelector('.more-content');
-    const scholarshipHeader = document.querySelector('.scholarship');
-    const buttons = document.querySelector('.buttons');
-    const button = document.querySelector('.buttons button:first-child');
-
-    if (!moreContent.classList.contains('show')) {
-        moreContent.classList.add('show');
-        scholarshipHeader.classList.add('expand'); 
-        button.textContent = 'See Less...';
-        buttons.classList.add('moved'); 
-    } else {
-        moreContent.classList.remove('show');
-        scholarshipHeader.classList.remove('expand'); 
-        button.textContent = 'See More...';
-        buttons.classList.remove('moved'); 
-    }
-}
-
 
